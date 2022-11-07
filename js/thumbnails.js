01bd01d547de9cc8c88490a8fa4b49fae3a11d77
ayasevich <artem.yasevich@mail.ru>
2022-11-07 23:19:13 +0300

    Refactor to use bigpicture module

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,19 +1,27 @@
 import {createPosts} from './data.js';
+import {adjustDisplayingAsBigPicture} from './bigpicture.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const picturesBlock = document.querySelector('.pictures');
 const picturesContainer = document.createDocumentFragment();
 
+function createThumbnail(post) {
+  const thumbnail = pictureTemplate.cloneNode(true);
+  thumbnail.querySelector('.picture__img').setAttribute('src', post.url);
+  const pictureInfo = thumbnail.querySelector('.picture__info');
+  pictureInfo.querySelector('.picture__comments').textContent = post.comments.length;
+  pictureInfo.querySelector('.picture__likes').textContent = post.likes;
+  return thumbnail;
+}
+
 const renderThumbnails = () => {
   for (const post of createPosts()) {
-    const thumbnail = pictureTemplate.cloneNode(true);
-    thumbnail.querySelector('.picture__img').setAttribute('src', post.url);
-    const pictureInfo = thumbnail.querySelector('.picture__info');
-    pictureInfo.querySelector('.picture__comments').textContent = post.comments.length;
-    pictureInfo.querySelector('.picture__likes').textContent = post.likes;
+    const thumbnail = createThumbnail(post);
+    adjustDisplayingAsBigPicture(thumbnail, post);
     picturesContainer.appendChild(thumbnail);
   }
   picturesBlock.appendChild(picturesContainer);
 };
+
 
 export {renderThumbnails};
