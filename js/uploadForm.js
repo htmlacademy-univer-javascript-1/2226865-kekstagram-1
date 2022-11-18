18462bc9f259c043a76eae82f5d51963349f8efc
ayasevich <artem.yasevich@mail.ru>
2022-11-19 00:02:18 +0300

    Implement form opening and closing

--- js/uploadForm.js
+++ js/uploadForm.js
@@ -1,6 +1,25 @@
+const ESC_KEYCODE = 27;
+
 const uploadForm = document.querySelector('.img-upload__overlay');
 const uploadPhotoInput = document.querySelector('#upload-file');
+const closeFormButton = document.querySelector('#upload-cancel');
 
 uploadPhotoInput.addEventListener('change', () => {
   uploadForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+});
+
+const closeForm = () => {
+  document.body.classList.remove('modal-open');
+  uploadForm.classList.add('hidden');
+  uploadPhotoInput.name = '';
+};
+
+closeFormButton.addEventListener('click', closeForm);
+
+document.addEventListener('keydown', (evnt) => {
+  if (evnt.keyCode === ESC_KEYCODE) {
+    evnt.preventDefault();
+    closeForm();
+  }
 });
