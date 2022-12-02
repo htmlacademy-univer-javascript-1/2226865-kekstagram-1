0279f1ccd1cc5ec1407a29469c67419fdda27c9c
ayasevich <artem.yasevich@mail.ru>
2022-12-02 13:23:39 +0300

    Fix debounce
    Changed the implementation to a subjectively more understandable one

--- js/util.js
+++ js/util.js
@@ -21,12 +21,11 @@ function random(min, max) {
   return Math.floor(result);
 }
 
+let timeoutId;
+
 function debounce(callback, timeoutDelay = 500) {
-  let timeoutId;
-  return (...rest) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-  };
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(callback, timeoutDelay);
 }
 
 function throttle(callback, delayBetweenFrames) {
